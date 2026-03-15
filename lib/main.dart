5790102066794773b8dec795915d3227d98fe789
wassimm12 <karimmay078@gmail.com>
2026-03-15 14:14:44 +0100

    link with fire base

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';       // 🔹 Import Firebase
+import 'firebase_options.dart';                           // 🔹 Options générées par flutterfire
 import 'package:mon_projet/screens/view_users.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();            // 🔹 Nécessaire pour async avant runApp
+  await Firebase.initializeApp(                          // 🔹 Initialise Firebase
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+  @overrid
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gestion des Utilisateurs by wassim',
